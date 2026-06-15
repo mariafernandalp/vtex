414b20c71209c0f7b4c4b49ce20620c6077958c0
mariafernandalp <maria.costa@larplasticos.com.br>
2026-06-15 17:07:01 -0300

    add new custom nav bar links

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,7 +1,9 @@
 import CarouselBanner from "./Banner/Banner";
 import Incentives from "./Incentives/Incentives";
+import NavbarCategories from "./NavbarCategories/NavbarCategories";
 
 export default {
     CarouselBanner,
-    Incentives
+    Incentives,
+    NavbarCategories
 }
