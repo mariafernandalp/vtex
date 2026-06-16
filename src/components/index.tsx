9f134742b2105e4d7ad7e55c2dd9230fa9c21e27
mariafernandalp <maria.costa@larplasticos.com.br>
2026-06-16 10:46:26 -0300

    feat: add whatsapp buton

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,9 +1,11 @@
 import CarouselBanner from "./Banner/Banner";
 import Incentives from "./Incentives/Incentives";
 import ProductShelfWithButton from "./sections/ProductShelfWithButton";
+import WhatsAppButton from "./WhatsAppButton/WhatsAppButton";
 
 export default {
     CarouselBanner,
     Incentives,
     ProductShelf: ProductShelfWithButton,
+    WhatsAppButton,
 };
